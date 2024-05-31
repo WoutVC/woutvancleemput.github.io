748a31e53b01eae21d4ff901732b75723f578fe2
Wout Van Cleemput <wout.vancleemput@student.hogent.be>
2024-05-31 19:32:57 +0200

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import "leaflet/dist/leaflet.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
